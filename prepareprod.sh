b96220d1001b57b18a5f02e8bce18210a04c07f4
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2023-06-25 21:00:21 +0700

    Replace prisma sqlite setup with postgresql

--- prepareprod.sh
+++ prepareprod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 pnpm init:envskipcheck
-cp prisma/schema.prod.prisma prisma/schema.prisma
 pnpm install 
-rm -rf prisma/migrations 
-mkdir -p prisma/migrations/0_init 
-npx prisma migrate diff --from-empty  --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql 
+# rm -rf prisma/migrations 
+# mkdir -p prisma/migrations/0_init 
+# npx prisma migrate diff --from-empty  --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql 
 npx prisma migrate deploy
---
+++ prisma/migrations/20230625135904_regenerate_migration_for_postgresql/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AuthToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "AuthToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Good" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "buyingPrice" DECIMAL(65,30) NOT NULL,
+    "sellingPrice" DECIMAL(65,30) NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Good_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AuthToken_token_key" ON "AuthToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Good_name_userId_key" ON "Good"("name", "userId");
+
+-- AddForeignKey
+ALTER TABLE "AuthToken" ADD CONSTRAINT "AuthToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Good" ADD CONSTRAINT "Good_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
