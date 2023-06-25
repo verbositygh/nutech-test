642e934d288f9519f0a1f95c15d6607793596612
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2023-06-25 20:35:36 +0700

    Fix init commands in prepareprod.sh

--- prepareprod.sh
+++ prepareprod.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+pnpm init:env 
 cp prisma/schema.prod.prisma prisma/schema.prisma
 rm .env
 pnpm install 
-pnpm init:env 
-pnpm init:prod 
 rm -rf prisma/migrations 
 mkdir -p prisma/migrations/0_init 
 npx prisma migrate diff --from-empty  --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql 
