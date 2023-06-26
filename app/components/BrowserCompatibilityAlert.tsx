a2fa30df1ad23eac288eb5f0bb14c1b26ed80445
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2023-06-26 14:30:09 +0700

    Fix document not found on statically generated contents

--- app/components/BrowserCompatibilityAlert.tsx
+++ app/components/BrowserCompatibilityAlert.tsx
@@ -1,9 +1,16 @@
 import { AlertTriangle } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const BrowserCompatibilityAlert = () => {
+  const [isBrowser, setIsBrowser] = useState(false);
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      setIsBrowser(true)
+    }
+  }, []);
   return (
     // @ts-ignore This is for compatibility purposes
-    (!document.createElement('dialog').showModal) ?
+    (isBrowser && !document.createElement('dialog').showModal) ?
       <section className={'mb-6'}>
         <div className={'bg-yellow-400 shadow-xl shadow-amber-100 text-zinc-800 flex items-center gap-4 text-sm font-medium leading-6 p-4 rounded'}>
           <AlertTriangle size={20} />
