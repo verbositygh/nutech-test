1fedfed5d9f6f764b629ec8aad143877f7e02b14
Louis Raymond <ilikebikes+gitlab-verbosity@femmund.com>
2023-06-26 14:49:17 +0700

    Fix compat detection should not run in ssr

--- app/components/BrowserCompatibilityAlert.tsx
+++ app/components/BrowserCompatibilityAlert.tsx
@@ -1,16 +1,9 @@
 import { AlertTriangle } from "lucide-react";
-import { useEffect, useState } from "react";
 
 const BrowserCompatibilityAlert = () => {
-  const [isBrowser, setIsBrowser] = useState(false);
-  useEffect(() => {
-    if (typeof window === 'undefined') {
-      setIsBrowser(true)
-    }
-  }, []);
   return (
     // @ts-ignore This is for compatibility purposes
-    (isBrowser && !document.createElement('dialog').showModal) ?
+    (!document.createElement('dialog').showModal) ?
       <section className={'mb-6'}>
         <div className={'bg-yellow-400 shadow-xl shadow-amber-100 text-zinc-800 flex items-center gap-4 text-sm font-medium leading-6 p-4 rounded'}>
           <AlertTriangle size={20} />
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@
 import useLogin from './hooks/useLogin';
 import UserAccount from './components/UserAccount';
 import GoodTable from './components/GoodTable';
-import BrowserCompatibilityAlert from './components/BrowserCompatibilityAlert';
+import dynamic from 'next/dynamic';
 
 export default function Home() {
   const login = useLogin();
+  const BrowserCompatibilityAlert = dynamic(() => import('./components/BrowserCompatibilityAlert'), {ssr: false});
   return (
     <main className="flex min-h-screen w-full flex-col sm:p-24 p-16 items-center">
       <div className={'flex flex-col max-w-3xl w-full'}>
-      <BrowserCompatibilityAlert />
+        <BrowserCompatibilityAlert />
         <section className={'mb-6'}>
           <UserAccount login={login} />
         </section>
